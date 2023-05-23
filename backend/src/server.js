2e48ebb2e5b452dbe40fcd2ddcdc60e1d46d230d
Victor Assis <victor_assis@poli.ufrj.br>
2023-05-22 23:27:17 -0300

    Implemenation of Cors at the backend.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 const routes = require("./products.routes");
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
@@ -10,7 +11,7 @@ app.get("/", (request, response) => {
   return response.send("Hello World!");
 });
 
-const portNumber = 3000;
+const portNumber = 3001;
 app.listen(portNumber, () =>
   console.log("🚀 Server is running. Listening on port " + portNumber)
 );
